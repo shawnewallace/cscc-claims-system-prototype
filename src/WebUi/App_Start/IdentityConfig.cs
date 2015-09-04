e2b5d945df4b20d91c3428cca125e4028a494ba4
Shawn Wallace <shawnewallace@gmail.com>
2015-09-04 15:07:37 -0400

    changed password rules

--- src/WebUi/App_Start/IdentityConfig.cs
+++ src/WebUi/App_Start/IdentityConfig.cs
@@ -56,14 +56,14 @@ namespace WebUi
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
-            manager.UserLockoutEnabledByDefault = true;
+            manager.UserLockoutEnabledByDefault = false;
             manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
             manager.MaxFailedAccessAttemptsBeforeLockout = 5;
 
